99920781235c7b44509666bfbb4d288f161bb17f
Zacck <coderv63@gmail.com>
2016-09-02 12:56:08 +0200

    changed express server to serve over http

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-//HTTPS TO HTTP
-app.use(function (req, res, next){
-  /*if over http
-  if(req.headers['x-forwarded-proto'] ===  'http') {
-    res.redirect('https://'+req.hostname +req.url);
-  } else {
-    next();
-  }
-  */
-});
+
 
 //show app folder to serve
 app.use(express.static('public')); // specifies a a folder name to expose
