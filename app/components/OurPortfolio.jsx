4acb5fc52207df7bc4dba06ff33aaa1021225d8c
Zacck <coderv63@gmail.com>
2016-10-05 10:46:18 +0200

    work on portfolio section

--- app/components/OurPortfolio.jsx
+++ app/components/OurPortfolio.jsx
@@ -8,14 +8,21 @@ export var OurPortfolio = React.createClass({
                    <hr className="port-border"/>
                  </div>
                  <div className="row">
-                   <div>
-
+                   <div className="port-item text-center columns small-12 medium-3 large-3">
+                     <img className="port-image" src={require('../images/web_design.jpg')}/>
+                     <p className="port-label">WEB DESIGN</p>
                    </div>
-                   <div>
-
+                   <div className="port-item text-center columns small-12 medium-3 large-3">
+                     <img className="port-image" src={require('../images/web_dev.jpg')}/>
+                     <p className="port-label">WEB DEVELOPMENT</p>
                    </div>
-                   <div>
-                     
+                   <div className="port-item text-center columns small-12 medium-3 large-3">
+                     <img className="port-image" src={require('../images/creative.jpg')}/>
+                     <p className="port-label">CONCEPTS</p>
+                   </div>
+                   <div className="port-item text-center columns small-12 medium-3 large-3">
+                     <img className="port-image" src={require('../images/mobile_apps_one.jpg')}/>
+                     <p className="port-label">MOBILE APPS</p>
                    </div>
                  </div>
 
