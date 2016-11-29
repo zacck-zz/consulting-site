68d54195958f04aa53b55055f9f0d8fde8ed2597
zacck <coderv63@gmail.com>
2016-11-29 16:27:37 +0200

    remove marike

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -16,11 +16,13 @@ var AboutUs = React.createClass({
             <p className="work-philosophy">I like finding simple solutions to very difficult problems</p>
           </div>
           <div className="small-12 medium-4 large-4 columns text-center">
+            {/*
             <img src={require('../images/marike_designer.jpg')} className="about-pic"/>
               <p className="profile-title">THE DESIGNER</p>
               <p className="profile-name">MARIKER CRONJE</p>
               <p className="work-philosophy">I love pretty things & making ugly things pretty</p>
           </div>
+          */}
           <div className="small-12 medium-4 large-4 columns text-center">
             <img src={require('../images/craig.jpg')} className="about-pic"/>
               <p className="profile-title">THE RUNNER</p>
