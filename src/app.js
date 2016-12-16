5eba57aa12079fd11ded5b615a34a25fc3b44643
zacck <coderv63@gmail.com>
2016-12-16 13:11:35 +0200

    install webpack 2 and configure to run and add start and build scripts to package

---
+++ src/app.js
@@ -0,0 +1,5 @@
+var _ = require('lodash');
+var array = [1];
+var other = _.concat(array, 2,[3],[[4]]);
+
+console.log(other);
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/src', //__dirname is the root of the project and src is the source
+  entry: {
+    app:'./app.js',
+  },
+  output: {
+    path: __dirname + '/dist', // /dist is the destination of the bundle
+    filename: '[name].bundle.js',
+  },
+};
