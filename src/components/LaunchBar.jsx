4b8e0c7ce63c0ea2ab4ca429103c72142cda6afa
zacck <coderv63@gmail.com>
2016-12-16 15:51:49 +0200

    work on adding options for app bar

--- src/components/LaunchBar.jsx
+++ src/components/LaunchBar.jsx
@@ -1,12 +1,28 @@
 import React, {Component} from 'react'
 import AppBar from 'material-ui/AppBar'
+import IconMenu from 'material-ui/IconMenu'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+import IconButton from 'material-ui/IconButton'
+import MenuItem from 'material-ui/MenuItem'
 
 export class LaunchBar extends Component {
   render() {
     return(
       <AppBar
         title="Launch Design"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
+        iconElementRight= {
+          <IconMenu
+            iconButtonElement={
+              <IconButton><MoreVertIcon /></IconButton>
+            }
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="About" />
+              <MenuItem primaryText="Portfolio" />
+              <MenuItem primaryText="Contact" />
+          </IconMenu>
+        }
       />
     );
   }
