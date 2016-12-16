c390544213dd80ca09cca0de3845d316b8238ccb
zacck <coderv63@gmail.com>
2016-12-16 15:25:26 +0200

    work on adjusting react event tap plugin to current react version compatible

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom'
 //add components use router for this
 import LaunchBar from 'LaunchBar'
 
+//add inject plugin for material-ui
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+
+//start inject pluging
+injectTapEventPlugin();
 ReactDOM.render(
   <div>
     <LaunchBar/>
