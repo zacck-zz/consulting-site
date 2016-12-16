56719f21ec38786a58da0432c97ef93a6c556aef
zacck <coderv63@gmail.com>
2016-12-16 14:55:44 +0200

    work on config to load the code from app.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   //wepack dumps it compiled output here
   output: {
-    path: __dirname + '/dist', // /dist is the destination of the bundle
+    path: __dirname + '/dist/assets', // /dist is the destination of the bundle
     filename: '[name].bundle.js',
     publicPath: '/assets', //This works with webpack-dev-server
   },
