4b469556b1b712f38652bccc370e6b0c42fd5c90
zacck <coderv63@gmail.com>
2016-12-16 15:15:57 +0200

    add components dir

---
+++ src/components/LaunchBar.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export class LaunchBar extends Component {
+  render() {
+    return(
+      <div>
+        <p>Launch Bar</p>
+      </div>
+    );
+  }
+};
+
+export default LaunchBar;
