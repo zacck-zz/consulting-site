1c7aeca99fdcf0042951aead85b369d3772d9dd2
zacck <coderv63@gmail.com>
2016-12-16 12:16:28 +0200

    work on contacts information

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,7 +7,7 @@ export var Footer = React.createClass({
                   <img className="footer-logo" src={require('../images/logo_launch_design.png')}/>
                   <p> Contact Us </p>
                   <p><a href='info@launchdesign.co.za'>info@launchdesign.co.za</a></p>
-                  <p><a href='+27 (082) 609 1179'>+27 (082) 609 1179</a></p>
+                  <p><a href='+27 (072) 434 1105'>+27 (072) 434 1105</a></p>
                 </div>
                </div>
              );
