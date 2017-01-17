0fe197247882a6c9cbbd413046d31596ea6a0ec1
zacck <coderv63@gmail.com>
2017-01-17 13:25:46 +0200

    work on https redirect

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-/*HTTPS TO HTTP
+/*HTTPS TO HTTP*/
 app.use(function (req, res, next){
   //if over http
   if(req.headers['x-forwarded-proto'] ===  'http') {
@@ -14,15 +14,12 @@ app.use(function (req, res, next){
     next();
   }
 });
-*/
+
 
 //show app folder to serve
 app.use(express.static(__dirname +'/dist')); // specifies a a folder name to expose
 
-//security stuff
-app.get('/.well-known/acme-challenge/:content', function(req, res){
-  res.send('1xIaU6DBhb9NeF9M31wcuPoZSqVVbAr3cb4a_ffug3s.zPkW8YHWcnFzxlXv4QBLG87lSCbw_KunsO076Bmk9y8')
-})
+
 
 //start the server
 app.listen(PORT, function(){ // takes the port you are serving to and a function
