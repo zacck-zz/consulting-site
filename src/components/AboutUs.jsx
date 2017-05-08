d024f0bc348e765beb5e4542fbf3df3db1d1e7c2
zacck <coderv63@gmail.com>
2017-05-08 11:50:04 +0200

    work on about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,39 +11,27 @@ export class AboutUs extends Component {
   render() {
     return (
       <div className="row about-us">
-        <div className="columns small-12 medium-4 large-4">
+        <div className="columns small-12 medium-6 large-6">
         <Card>
           <CardHeader
             title="Zacck Osiemo"
             avatar={require("../images/zack_web_developer.jpg")}
             >
           </CardHeader>
-          <CardTitle title="The Web Developer"></CardTitle>
+          <CardTitle title="Developer"></CardTitle>
           <CardText>
             I like finding simple solutions to very difficult problems.
           </CardText>
         </Card>
       </div>
-      <div className="columns small-12 medium-4 large-4">
-        <Card>
-          <CardHeader
-          title="Cheslin Siebritz"
-          avatar={require("../images/coding.jpg")}
-          ></CardHeader>
-          <CardTitle title="The Designer"></CardTitle>
-            <CardText>
-              I like making things pretty.
-            </CardText>
-        </Card>
-      </div>
-      <div className="columns small-12 medium-4 large-4">
+      <div className="columns small-12 medium-6 large-6">
         <Card>
           <CardHeader
             title="Craig Magada"
             avatar={require('../images/craig.jpg')}
             >
           </CardHeader>
-          <CardTitle title="The Runner"></CardTitle>
+          <CardTitle title="Runner"></CardTitle>
           <CardText>
             I like making sure everything happens on time.
           </CardText>
