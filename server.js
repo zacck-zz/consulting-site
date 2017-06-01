c25b7f6c228bb4a6d7442b228a3b4d9d75b1041a
zacck <coderv63@gmail.com>
2017-06-01 10:13:58 +0200

    Work on updating heroku certicates and adding ssl redirect for app.

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// /*HTTPS TO HTTP*/
-// app.use(function (req, res, next){
-//   //if over http
-//   if(req.headers['x-forwarded-proto'] ===  'http') {
-//     res.redirect('https://'+req.hostname +req.url);
-//   } else {
-//     next();
-//   }
-// });
-
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
- res.send('oh_xjDO3ePZjUOxy80998UqUFwYgnn8lduRqjZlwH-w.zPkW8YHWcnFzxlXv4QBLG87lSCbw_KunsO076Bmk9y8')
+/*HTTPS TO HTTP*/
+app.use(function (req, res, next){
+  //if over http
+  if(req.headers['x-forwarded-proto'] ===  'http') {
+    res.redirect('https://'+req.hostname +req.url);
+  } else {
+    next();
+  }
 });
-
 
 //show app folder to serve
 app.use(express.static(__dirname +'/dist')); // specifies a a folder name to expose
