ed2dcba89b1149c9ffa5478b8c89d2aa6aee8b10
zacck <coderv63@gmail.com>
2017-08-29 14:43:52 +0200

    disable https  and add message

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-/*HTTPS TO HTTP*/
+/*HTTPS TO HTTP
 app.use(function (req, res, next){
   //if over http
   if(req.headers['x-forwarded-proto'] ===  'http') {
@@ -14,14 +14,13 @@ app.use(function (req, res, next){
     next();
   }
 });
-
+*/
 //show app folder to serve
 app.use(express.static(__dirname +'/dist')); // specifies a a folder name to expose
 
 app.get('/.well-known/acme-challenge/:content', function(req, res) {
-	res.send('jLYarHD3bocQqIQjZWRdLYjPPXmCGHD2KiU7LHhF4.zPkW8YHWcnFzxlXv4QBLG87lSCbw_KunsO076Bmk9y8')
+	res.send('5pjFiu3VTDcQWYMb7a50tTXaEUlgg0HNKFIA4FTEFYo.zPkW8YHWcnFzxlXv4QBLG87lSCbw_KunsO076Bmk9y8')
 });
-
 //start the server
 app.listen(PORT, function(){ // takes the port you are serving to and a function
   console.log('Express Server is up on port ' + PORT);//log top console
